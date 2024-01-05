7d63a6b32088567b14932b2409304e99902f0a5f
peter <sddlpeter@gmail.com>
2024-01-05 14:02:39 -0800

    first commit

---
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atguigu.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class GulimallProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
