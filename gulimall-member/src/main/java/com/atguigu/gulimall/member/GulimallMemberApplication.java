4567f95c0142db5fb987a8fd2c32f75593fd5baa
peter <sddlpeter@gmail.com>
2024-01-08 13:33:50 -0800

    commit

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
@@ -3,7 +3,12 @@ package com.atguigu.gulimall.member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+// 1. 引入openfeign， 2. 编写一个接口，告诉springcloud这个接口要调用远程服务
+// 3. 开启远程调用功能
+
+@EnableFeignClients(basePackages = "com.atguigu.gulimall.member.feign")
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallMemberApplication {
