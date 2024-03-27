6c103bbf10e723c91f02fb5d84bfe6085504e304
peter <sddlpeter@gmail.com>
2024-03-27 10:39:32 -0700

    order module

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
@@ -4,14 +4,14 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableRedisHttpSession
 @EnableRabbit
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallOrderApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GulimallOrderApplication.class, args);
 	}
-
 }
